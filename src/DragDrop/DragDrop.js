a4dcbcd3873f1186dfdb7a80a7c383f736017cb0
Perry Angelora <peamaillist@gmail.com>
2022-02-02 18:42:40 -0500

    Added dragEnter and dragLeave tests

--- src/DragDrop/DragDrop.js
+++ src/DragDrop/DragDrop.js
@@ -6,11 +6,14 @@ export default function DragDrop({ children, options = {} }) {
   const activeClassDrag = useRef(undefined);
   const activeIdDragElement = useRef(undefined);
   const matchHash = {};
+  const fileCallback = options.fileCallback ? options.fileCallback : null;
+  const dragActive = options.customClass !== undefined ? options.customClass : 'drag-active';
+  const watchDrag = options.watchDrag || '*';
 
   useEffect(() => {
-    if (options.watchDrag instanceof Array) {
-      for (let i = 0; i < options.watchDrag.length; i += 1) {
-        matchHash[options.watchDrag[i]] = options.watchDrag[i];
+    if (watchDrag instanceof Array) {
+      for (let i = 0; i < watchDrag.length; i += 1) {
+        matchHash[watchDrag[i]] = watchDrag[i];
       }
     }
   }, []);
@@ -24,33 +27,66 @@ export default function DragDrop({ children, options = {} }) {
    * @param {Object} e DOM event object
    */
   function handleOnDragOver(e) {
-    e.preventDefault();
-    // watchDrag passed into options is an Array of class names
-    if (options.watchDrag !== '*') {
-      for (let i = 0; i < e.target.classList.length; i += 1) {
-        if (typeof matchHash[e.target.classList[i]] !== 'undefined') {
-          if (!activeClassDrag.current) {
-            activeClassDrag.current = document.querySelectorAll(`.${e.target.classList[i]}`);
-            for (let j = 0; j < activeClassDrag.current.length; j += 1) {
-              activeClassDrag.current[j].classList.add('drag-active');
+    if (watchDrag !== 'none') {
+      e.preventDefault();
+      // watchDrag passed into options is an Array of class names
+      if (watchDrag !== '*') {
+        for (let i = 0; i < e.target.classList.length; i += 1) {
+          if (typeof matchHash[e.target.classList[i]] !== 'undefined') {
+            if (!activeClassDrag.current) {
+              activeClassDrag.current = document.querySelectorAll(`.${e.target.classList[i]}`);
+              for (let j = 0; j < activeClassDrag.current.length; j += 1) {
+                activeClassDrag.current[j].classList.add(dragActive);
+              }
             }
           }
         }
       }
-    }
-    // id is passed into options as true
-    if (options.watchDrag === '*' && e.target.id) {
-      const dragIdElement = document.getElementById(e.target.id);
-      if (activeIdDragElement.current !== dragIdElement) {
-        dragIdElement.classList.add('drag-active');
+      // id is passed into options as true
+      if (watchDrag === '*' && e.target.id) {
+        const dragIdElement = document.getElementById(e.target.id);
+        if (activeIdDragElement.current !== dragIdElement) {
+          dragIdElement.classList.add(dragActive);
+        }
+        activeIdDragElement.current = dragIdElement;
       }
-      activeIdDragElement.current = dragIdElement;
     }
   }
 
+  /**
+   * If we're appending to file callback, we look at what has been requested and
+   * add them to the tempPayload Object, which is then added to the callbackPayload Object.
+   * @param {Node} e Node element
+   * @returns payload Object.
+   */
+  function handleAppendCallbackPayload(e) {
+    const tempPayload = {};
+    const toAppend = options.appendToFileCallback;
+    if (toAppend.includes('id')) {
+      tempPayload.id = e.target.id;
+    }
+    if (toAppend.includes('className')) {
+      tempPayload.className = e.target.className.replace(dragActive, '').split(' ').filter(Boolean);
+    }
+    if (toAppend.includes('data')) {
+      tempPayload.data = { ...e.target.dataset };
+    }
+    return tempPayload;
+  }
+
+  /**
+   * Drop Event Handler
+   */
   function handleDropEvent(e) {
     e.stopPropagation();
     e.preventDefault();
+    let callbackPayload;
+    if (options.appendToFileCallback) {
+      callbackPayload = handleAppendCallbackPayload(e);
+    }
+
+    callbackPayload.files = e.dataTransfer.files;
+    if (fileCallback) { fileCallback(callbackPayload); }
     handleDragLeave();
   }
 
@@ -58,22 +94,24 @@ export default function DragDrop({ children, options = {} }) {
    * Method that removes the class name appended on drag over.
    */
   function handleDragLeave() {
-    if (activeClassDrag.current) {
-      for (let i = 0; i < activeClassDrag.current.length; i += 1) {
-        activeClassDrag.current[i].classList.remove('drag-active');
+    if (watchDrag !== 'none') {
+      if (activeClassDrag.current) {
+        for (let i = 0; i < activeClassDrag.current.length; i += 1) {
+          activeClassDrag.current[i].classList.remove(dragActive);
+        }
+        activeClassDrag.current = undefined;
       }
-      activeClassDrag.current = undefined;
-    }
-    if (activeIdDragElement.current) {
-      activeIdDragElement.current.classList.remove('drag-active');
-      activeIdDragElement.current = undefined;
+      if (activeIdDragElement.current) {
+        activeIdDragElement.current.classList.remove(dragActive);
+        activeIdDragElement.current = undefined;
+      }
     }
   }
 
   return (
     <div
       className="drag-element"
-      onDragOver={(e) => handleOnDragOver(e)}
+      onDragOver={(e) => { handleOnDragOver(e); }}
       onDrop={(e) => handleDropEvent(e)}
       onDragLeave={handleDragLeave}
     >
--- tests/DragDrop.test.js
+++ tests/DragDrop.test.js
@@ -1,11 +1,145 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { render, screen, fireEvent, createEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import 'regenerator-runtime/runtime';
 import DragDrop from '../src/DragDrop/DragDrop';
+import AppTest from './AppTest';
 
-describe('File Drag and Drop Component', () => {
-  it('Renders without crashing', () => {
+// ***************************** Smoke ***************************** //
+describe('File Drag and Drop Component.', () => {
+  it('Renders without crashing.', () => {
     const div = document.createElement('DIV');
     ReactDOM.render(<DragDrop />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+// ***************** Drag File Over & Custom Class ***************** //
+describe('Drag File Over', () => {
+  const testCases = [
+    {
+      description: 'Should append "drag-active" on dragOver.',
+      options: {
+        watchDrag: '*',
+      },
+      expect: 'drag-active',
+    },
+    {
+      description: 'Should append "drag-active" on dragOver.',
+      options: {},
+      expect: 'drag-active',
+    },
+    {
+      description: 'Should append "drag-active" on dragOver.',
+      options: {
+        watchDrag: ['drop-list-item'],
+      },
+      expect: 'drag-active',
+    },
+    {
+      description: 'Should append "custom-class" on dragOver.',
+      options: {
+        customClass: 'custom-class',
+      },
+      expect: 'custom-class',
+    },
+  ];
+  testCases.forEach((test) => {
+    it(test.description, () => {
+      render(
+        <DragDrop options={test.options}>
+          <AppTest />
+        </DragDrop>,
+      );
+      const listElement = screen.getByText(/List Item Three/i);
+      fireEvent.dragOver(listElement);
+      expect(listElement).toHaveClass(test.expect);
+    });
+  });
+
+  it("Should append all elements with same class with 'drag-active.'", () => {
+    render(
+      <DragDrop options={{ watchDrag: ['first'] }}>
+        <AppTest />
+      </DragDrop>,
+    );
+    const elementDraggedOver = screen.getByText(/List Item Three/i);
+    fireEvent.dragOver(elementDraggedOver);
+    const listElements = screen.getAllByTestId('first');
+    listElements.forEach((element) => {
+      expect(element).toHaveClass('drag-active');
+    });
+  });
+
+  it('Should not append anything on dragOver.', () => {
+    render(
+      <DragDrop options={{ watchDrag: 'none' }}>
+        <AppTest />
+      </DragDrop>,
+    );
+    const listElement = screen.getByText(/List Item Three/i);
+    fireEvent.dragOver(listElement);
+    expect(listElement).not.toHaveClass('drag-active');
+  });
+});
+
+// ************************ Drag File Leave ************************ //
+describe('Drag File Leave', () => {
+  const testCases = [
+    {
+      description: 'Should remove class "drag-active" on dragLeave.',
+      options: {
+        watchDrag: '*',
+      },
+      expect: 'drag-active',
+    },
+    {
+      description: 'Should remove class "drag-active" on dragLeave.',
+      options: {},
+      expect: 'drag-active',
+    },
+    {
+      description: 'Should remove class "drag-active" on dragLeave.',
+      options: {
+        watchDrag: ['drop-list-item'],
+      },
+      expect: 'drag-active',
+    },
+    {
+      description: 'Should remove class "custom-class" on dragLeave.',
+      options: {
+        customClass: 'custom-class',
+      },
+      expect: 'custom-class',
+    },
+  ];
+  testCases.forEach((test) => {
+    it(test.description, () => {
+      render(
+        <DragDrop options={test.options}>
+          <AppTest />
+        </DragDrop>,
+      );
+      const listElement = screen.getByText(/List Item Four/i);
+      fireEvent.dragOver(listElement);
+      fireEvent.dragLeave(listElement);
+      expect(listElement).not.toHaveClass('drag-active');
+    });
+  });
+
+  it("Should remove 'drag-active' from all elements with same class", () => {
+    render(
+      <DragDrop options={{ watchDrag: ['first'] }}>
+        <AppTest />
+      </DragDrop>,
+    );
+    const elementDraggedOver = screen.getByText(/List Item Three/i);
+    fireEvent.dragOver(elementDraggedOver);
+    fireEvent.dragLeave(elementDraggedOver);
+    const listElements = screen.getAllByTestId('first');
+    listElements.forEach((element) => {
+      expect(element).not.toHaveClass('drag-active');
+    });
   });
 });
