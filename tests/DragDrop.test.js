0c5472e87b71153cd727d98faf67969aaca270c9
Perry Angelora <peamaillist@gmail.com>
2022-02-03 17:04:26 -0500

    Added tests for dropping file and appendToFileCallback potentialities

--- tests/DragDrop.test.js
+++ tests/DragDrop.test.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { render, screen, fireEvent, createEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import 'regenerator-runtime/runtime';
 import DragDrop from '../src/DragDrop/DragDrop';
-import AppTest from './AppTest';
+import {
+  AppTest,
+  AppTestNoId,
+  AppTestNoClass,
+  AppTestNoData,
+} from './AppTest';
 
 // ***************************** Smoke ***************************** //
 describe('File Drag and Drop Component.', () => {
@@ -140,6 +145,114 @@ describe('Drag File Leave', () => {
     const listElements = screen.getAllByTestId('first');
     listElements.forEach((element) => {
       expect(element).not.toHaveClass('drag-active');
+    });
+  });
+});
+
+// *************** Drop File and appendToFileCallback *************** //
+describe('Drop File - appendToFileCallback', () => {
+  const testList = [
+    {
+      description: 'Should return file to callback',
+      options: {},
+      expect: (data) => expect(data).toStrictEqual({
+        files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+      }),
+    },
+    {
+      description: 'Should return file and className to callback',
+      options: { appendToFileCallback: ['className'] },
+      expect: (data) => expect(data).toStrictEqual({
+        className: ['drop-list-item', 'second', 'which'],
+        files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+      }),
+    },
+    {
+      description: 'Should return file and id to callback',
+      options: { appendToFileCallback: ['id'] },
+      expect: (data) => expect(data).toStrictEqual({
+        id: 'listItemFour',
+        files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+      }),
+    },
+    {
+      description: 'Should return file and data to callback',
+      options: { appendToFileCallback: ['data'] },
+      expect: (data) => expect(data).toStrictEqual({
+        data: { foo: 'listWhich', bar: 'listItemFour' },
+        files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+      }),
+    },
+    {
+      description: 'Should return file and id, className and data to callback',
+      options: { appendToFileCallback: ['data', 'id', 'className'] },
+      expect: (data) => expect(data).toStrictEqual({
+        id: 'listItemFour',
+        className: ['drop-list-item', 'second', 'which'],
+        data: { foo: 'listWhich', bar: 'listItemFour' },
+        files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+      }),
+    },
+  ];
+
+  testList.forEach((test) => {
+    it(test.description, (done) => {
+      function callback(data) {
+        test.expect(data);
+      }
+      done();
+      render(
+        <DragDrop options={{ ...test.options, fileCallback: callback }}>
+          <AppTest />
+        </DragDrop>,
+      );
+      const dropArea = screen.getByText(/List Item Four/i);
+      fireEvent.drop(dropArea, {
+        dataTransfer: {
+          files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+        },
+      });
+    });
+  });
+
+  const testListAppend = [
+    {
+      description: "Should return null if 'id' is passed to appendToFileCallback, but isn't in DOM on drop",
+      options: { appendToFileCallback: ['id'] },
+      expect: 'id',
+      currentDOM: AppTestNoId,
+    },
+    {
+      description: "Should return null if 'data' is passed to appendToFileCallback, but isn't in DOM on drop",
+      options: { appendToFileCallback: ['data'] },
+      expect: 'data',
+      currentDOM: AppTestNoData,
+    },
+    {
+      description: "Should return null if 'className' is passed to appendToFileCallback, but isn't in DOM on drop",
+      options: { appendToFileCallback: ['className'] },
+      expect: 'className',
+      currentDOM: AppTestNoClass,
+    },
+  ];
+
+  testListAppend.forEach((test) => {
+    it(test.description, (done) => {
+      function callback(data) {
+        expect(data[test.expect]).toBeNull();
+      }
+      done();
+      render(
+        <DragDrop options={{ ...test.options, fileCallback: callback }}>
+          <test.currentDOM />
+        </DragDrop>,
+      );
+      const dropArea = screen.getByText(/List Item Four/i);
+      fireEvent.drop(dropArea, {
+        dataTransfer: {
+          files: [new File(['ABCDEF'], 'myImageFile.png', { type: 'image/png' })],
+        },
+      });
     });
   });
 });
